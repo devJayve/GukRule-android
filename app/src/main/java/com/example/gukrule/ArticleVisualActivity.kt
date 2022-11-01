e0234af41a672d16d4482ac382f0d48769fedb5e
BlowUP1997 <sksmsal97@gmail.com>
2022-11-01 20:07:08 +0900

    11/1 진행중 - 데이터 받아오도록 구조 짜야함 + bottom sheet 동작 미완성

--- app/src/main/java/com/example/gukrule/ArticleVisualActivity.kt
+++ app/src/main/java/com/example/gukrule/ArticleVisualActivity.kt
@@ -1,4 +1,13 @@
 package com.example.gukrule
 
-class ArticleVisualActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ArticleVisualActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article_visual)
+
+    }
 }
