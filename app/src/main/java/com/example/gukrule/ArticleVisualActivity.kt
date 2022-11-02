7dc066eb89150157e804ff22c2221f137621e9ca
BlowUP1997 <sksmsal97@gmail.com>
2022-11-02 09:49:57 +0900

    11/2시작 전 저장

--- app/src/main/java/com/example/gukrule/ArticleVisualActivity.kt
+++ app/src/main/java/com/example/gukrule/ArticleVisualActivity.kt
@@ -3,11 +3,20 @@ package com.example.gukrule
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.gukrule.databinding.ActivityMainBinding
 
 class ArticleVisualActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
         setContentView(R.layout.activity_article_visual)
+
+
 
     }
 }
