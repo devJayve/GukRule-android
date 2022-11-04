64aaaaefb2a6e27b1e002b128cda8a4969dffe58
devdejavu <wndud4576@naver.com>
2022-11-04 23:00:41 +0900

    Add VisualBudgetRV

--- app/src/main/java/com/example/gukrule/VisualActivity.kt
+++ app/src/main/java/com/example/gukrule/VisualActivity.kt
@@ -1,11 +1,21 @@
 package com.example.gukrule
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.gukrule.adapter.CarouselRVAdapter
+import com.example.gukrule.adapter.VisualBudgetRVAdapter
+import com.example.gukrule.data.BudgetData
 import com.example.gukrule.databinding.ActivityVisualBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.sothree.slidinguppanel.SlidingUpPanelLayout
+import com.sothree.slidinguppanel.SlidingUpPanelLayout.PanelState
 
 class VisualActivity : AppCompatActivity() {
     private lateinit var binding: ActivityVisualBinding
@@ -16,7 +26,59 @@ class VisualActivity : AppCompatActivity() {
         binding = ActivityVisualBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.visualPanel.isTouchEnabled = false
+        binding.visualPanel.addPanelSlideListener(PanelEventListener())
 
+        setSupportActionBar(binding.budgetToolbar)
+        supportActionBar?.apply {
+            setIcon(R.drawable.tmp_resize)
+            setDisplayUseLogoEnabled(true)
+            title = "시각화"
+        }
 
+        var dataList = ArrayList<BudgetData>()
+
+        for(i: Int in 1..20) {
+            dataList.add(
+                BudgetData(
+                budget_icon = "test",
+                budget_title = "this title index is $i",
+                program_name = "$i 번째 프로그램"
+            ))
+        }
+
+        val visualBudgetRVAdapter = VisualBudgetRVAdapter(dataList)
+        val budgetRV = binding.budgetRV
+        budgetRV.adapter = visualBudgetRVAdapter
+        binding.budgetRV.layoutManager= LinearLayoutManager(this)
+
+        visualBudgetRVAdapter.setItemClickListener(object : VisualBudgetRVAdapter.OnItemClickListener{
+            override fun onClick(v: View, position: Int) {
+                binding.visualPanel.panelState = PanelState.COLLAPSED
+            }
+        })
+    }
+
+    // 이벤트 리스너
+    inner class PanelEventListener : SlidingUpPanelLayout.PanelSlideListener {
+        // 패널이 슬라이드 중일 때
+        override fun onPanelSlide(panel: View?, slideOffset: Float) {
+
+        }
+
+        // 패널의 상태가 변했을 때
+        override fun onPanelStateChanged(panel: View?, previousState: PanelState?, newState: PanelState?) {
+            if (newState == PanelState.COLLAPSED) {
+                binding.budgetRV.visibility = View.GONE
+                binding.visualPanel.isTouchEnabled = true
+//                binding.programListTitle.text = "다른거 보려면 클릭하세요"
+//                binding.programListTitle.gravity = Gravity.CENTER
+            } else if (newState == PanelState.EXPANDED) {
+                binding.budgetRV.visibility = View.VISIBLE
+                binding.visualPanel.isTouchEnabled = false
+//                binding.programListTitle.text = "국회 프로그램 목록"
+//                binding.programListTitle.gravity = Gravity.START
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/gukrule/adapter/VisualBudgetRVAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.gukrule.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gukrule.R
+import com.example.gukrule.data.BudgetData
+
+class VisualBudgetRVAdapter(private val budgetList: ArrayList<BudgetData>)
+    : RecyclerView.Adapter<VisualBudgetRVAdapter.VisualBudgetHolder>() {
+
+    private lateinit var itemClickListener : OnItemClickListener
+
+    class VisualBudgetHolder(view: View): RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VisualBudgetHolder {
+        val viewHolder = LayoutInflater.from(parent.context).inflate(R.layout.item_budget, parent, false)
+        return VisualBudgetRVAdapter.VisualBudgetHolder(viewHolder)
+    }
+
+    override fun onBindViewHolder(holder: VisualBudgetHolder, position: Int) {
+        val budgetIcon = holder.itemView.findViewById<ImageView>(R.id.budget_icon)
+        val category1 = holder.itemView.findViewById<TextView>(R.id.category_1)
+        val content = holder.itemView.findViewById<TextView>(R.id.content)
+
+        //budgetIcon.setImageDrawable()
+        category1.text = budgetList[position].program_name
+        content.text = budgetList[position].budget_title
+
+        // 리스트 내 budget card 클릭 이벤트
+        holder.itemView.setOnClickListener {
+            itemClickListener.onClick(it, position)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return budgetList.size
+    }
+
+    interface OnItemClickListener {
+        fun onClick(v: View, position: Int)
+    }
+
+    fun setItemClickListener(onItemClickListener: OnItemClickListener) {
+        this.itemClickListener = onItemClickListener
+    }
+
+}
