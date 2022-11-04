3062c3916e6908de9776b556f01e49d70f07283a
devdejavu <wndud4576@naver.com>
2022-11-05 02:31:33 +0900

    Add host open api

--- app/src/main/java/com/example/gukrule/VisualActivity.kt
+++ app/src/main/java/com/example/gukrule/VisualActivity.kt
@@ -1,8 +1,11 @@
 package com.example.gukrule
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.AttributeSet
+import android.util.Log
 import android.view.Gravity
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -13,9 +16,18 @@ import com.example.gukrule.adapter.CarouselRVAdapter
 import com.example.gukrule.adapter.VisualBudgetRVAdapter
 import com.example.gukrule.data.BudgetData
 import com.example.gukrule.databinding.ActivityVisualBinding
+import com.example.gukrule.retrofit.DetailBudgetData
+import com.example.gukrule.retrofit.DetailBudgetList
+import com.example.gukrule.retrofit.JsonRowList
+import com.example.gukrule.retrofit.RetrofitClient
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.gson.Gson
+import com.google.gson.JsonObject
 import com.sothree.slidinguppanel.SlidingUpPanelLayout
 import com.sothree.slidinguppanel.SlidingUpPanelLayout.PanelState
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.create
 
 class VisualActivity : AppCompatActivity() {
     private lateinit var binding: ActivityVisualBinding
@@ -36,7 +48,7 @@ class VisualActivity : AppCompatActivity() {
             title = "시각화"
         }
 
-        var dataList = ArrayList<BudgetData>()
+        val dataList = ArrayList<BudgetData>()
 
         for(i: Int in 1..20) {
             dataList.add(
@@ -54,10 +66,36 @@ class VisualActivity : AppCompatActivity() {
 
         visualBudgetRVAdapter.setItemClickListener(object : VisualBudgetRVAdapter.OnItemClickListener{
             override fun onClick(v: View, position: Int) {
+                hostRetrofitClient()
                 binding.visualPanel.panelState = PanelState.COLLAPSED
             }
         })
+
+
+
     }
+
+    private fun hostRetrofitClient() {
+        var retrofit = RetrofitClient.initCongressRetrofit()
+        var budgetDetailApi = retrofit.create(RetrofitClient.BudgetApi::class.java)
+        budgetDetailApi.getDetailBusiness(fsclYY = "2020")
+            .enqueue(object : retrofit2.Callback<DetailBudgetList> {
+            override fun onResponse(
+                call: Call<DetailBudgetList>,
+                response: Response<DetailBudgetList>
+            ) {
+                val jsonResponse: JsonObject = response.body()!!.njzofberazvhjncha[1]
+                val jsonRowList = Gson().fromJson(jsonResponse, JsonRowList::class.java)
+                val finalData = Gson().fromJson(jsonRowList.row[0], DetailBudgetData::class.java)
+                Log.d("success", finalData.toString())
+            }
+
+            override fun onFailure(call: Call<DetailBudgetList>, t: Throwable) {
+                Log.d("failure", t.message.toString())
+            }
+        })
+    }
+
 
     // 이벤트 리스너
     inner class PanelEventListener : SlidingUpPanelLayout.PanelSlideListener {
--- app/src/main/java/com/example/gukrule/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/example/gukrule/retrofit/RetrofitClient.kt
@@ -1,6 +1,7 @@
 package com.example.gukrule.retrofit
 
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import okhttp3.OkHttpClient
@@ -16,11 +17,15 @@ object RetrofitClient {
     // 레트로핏 클라이언트 가져오기
 
     fun initCongressRetrofit(): Retrofit {
-        val url = "https://open.assembly.go.kr/portal/openapi" //서버 주소
+        val url = "https://open.assembly.go.kr/portal/openapi/" //서버 주소
+        val gson : Gson = GsonBuilder()
+            .setLenient()
+            .create()
+
         return Retrofit.Builder()
             .baseUrl(url)
             .client(OkHttpClient.Builder().build())
-            .addConverterFactory(GsonConverterFactory.create(Gson()))
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
     }
 
@@ -37,20 +42,20 @@ object RetrofitClient {
     }
 
     interface BudgetApi {
-        @GET("/njzofberazvhjncha")
+        @GET("njzofberazvhjncha")
         fun getDetailBusiness(
-            @Query("Key") key:String = "ce1a19a050384ee5ba1d3e4fecd7d8ec", // 인증키
+            @Query("KEY") key:String = "ce1a19a050384ee5ba1d3e4fecd7d8ec", // 인증키
             @Query("Type") type:String = "json",            // 호출문서(xml,json)
-            @Query("pIndex") pIndex:Int,                    // 페이지 위치
-            @Query("pSize") pSize:Int,                      // 페이지 당 요청 숫자
+//            @Query("pIndex") pIndex:Int? = null,                    // 페이지 위치
+//            @Query("pSize") pSize:Int? = null,                      // 페이지 당 요청 숫자
             @Query("FSCL_YY") fsclYY:String,                // 회계년도
-            @Query("EXP_DATE") exeDate:String? = null,      // 집행일
+            @Query("EXE_DATE") exeDate:String? = null,      // 집행일
             @Query("FSCL_NM") fsclName:String? = null,      // 회계명
             @Query("FLD_NM") fldName:String? = null,        // 분야명
             @Query("SECT_NM") sectName:String? = null,      // 부문명
             @Query("PGM_NM") pgmName:String? = null,        // 프로그램명
             @Query("ACTV_NM") actvName:String? = null,      // 단위사업명
-        ) : Call<DetailBudgetData>
+        ) : Call<DetailBudgetList>
     }
 
 //    /** Login Api (worked by KJY) **/
